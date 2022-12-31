0a10e2c08599979a5b9e522c6ddef3a3adf0eaa6
류채연 <haha03270@gmail.com>
2022-12-31 23:29:43 +0900

    [1231] 웹캠(카메라 불러오기) 및 사용법 설명 기능 구현 완료

--- js/script.js
+++ js/script.js
@@ -11,3 +11,47 @@ $(document).ready(function(){
   });
 
 });
+
+
+// 사용법
+$(function (){
+	$("#upload-tip_btn").click(function (){
+  	$(".upload_txt").toggle();
+  });
+});
+
+$(function (){
+	$("#camera-tip_btn").click(function (){
+  	$(".camera_txt").toggle();
+  });
+});
+
+
+// 웹캠
+// Init camera
+function camInit(stream) {
+  var cameraView = document.getElementById("cameraview");
+  cameraView.srcObject = stream;
+  cameraView.play();
+}
+
+function camInitFailed(error) {
+  console.log("get camera permission failed : ", error)
+}
+
+// Main init
+
+function mainInit() {
+  // Check navigator media device available
+  if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia )
+  {
+      // Navigator mediaDevices not supported
+      alert("Media Device not supported")
+      return;
+  }
+
+  navigator.mediaDevices.getUserMedia({video:true})
+      .then(camInit)
+      .catch(camInitFailed);
+
+}
